390bd919d56fa132b97faf88ea4b5c6f19f19a55
Yunus Emre OZAN <yemreozan@outlook.com>
2019-02-08 23:11:36 +0300

    Updates

---
+++ precache-manifest.c844bdb5c2803fc0ae4ef4f74b021700.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "abd5010a18cefb64834d",
+    "url": "/weather-app/static/js/main.abd5010a.chunk.js"
+  },
+  {
+    "revision": "9fc633992ce8f35eeb9f",
+    "url": "/weather-app/static/js/1.9fc63399.chunk.js"
+  },
+  {
+    "revision": "abd5010a18cefb64834d",
+    "url": "/weather-app/static/css/main.59fde5ed.chunk.css"
+  },
+  {
+    "revision": "97440d1bce06110c831bcd20a5634654",
+    "url": "/weather-app/index.html"
+  }
+];
